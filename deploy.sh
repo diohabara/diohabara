fe942b824b990b64caebe9c5c3b50c333998d166
tuampo <diohabara@gmail.com>
2022-09-10 14:46:54 -0500

    feat(resume): update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 tectonic -X build
-
 git add .
 git commit -m "feat(resume): update"
 cp -f ./build/resume/resume.pdf ~/Downloads/resume"$(date +%Y%m%d).pdf"
 cp -f ./build/resume/resume.pdf ~/Dropbox/resume"$(date +%Y%m%d).pdf"
+git push
