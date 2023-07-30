4d3690955ffb174497979ece04e7392abdccb930
=510 <diohabara@gmail.com>
2023-07-30 00:29:53 -0500

    Deleting deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-tectonic -X build
-git add .
-git commit -m "feat(resume): update"
-cp -f ./build/resume/resume.pdf ~/Downloads/resume"$(date +%Y%m%d).pdf"
-cp -f ./build/resume/resume.pdf ~/Dropbox/resume/resume"$(date +%Y%m%d).pdf"
-git push
